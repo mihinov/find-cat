e8073b087501527af1d478946e46bba2396f383e
Kamahin Mihail <kamahinmihail@gmail.com>
2020-04-05 21:18:01 +0300

    123

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const result = body.querySelector('.result');
 const resultSpan = result.querySelector('span');
 const overlay = body.querySelector('.overlay');
 const overlaySpan = overlay.querySelector('span');
-const pogresh = 50;
+const pogresh = 25;
 let heightWindow, widthWindow,
 randomWidth, randomHeight,
 leftCat, topCat, count, audio, findCatFlag;
